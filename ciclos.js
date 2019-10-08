5fbe9ee7a70c4d488dd20d742b675915dc4628c6
eparionad <eparionad@gmail.com>
2019-10-07 19:44:14 -0500

    se termino los ejercicios

---
+++ ciclos.js
@@ -0,0 +1,51 @@
+//Ejercicio 1
+/*for(i=1;i<21;i++){
+    document.write(i+"<br>");
+}*/
+
+//Ejercicio 2
+/*for(i=1;i<=30;i+=3){
+    document.write(i+"<br>");
+}*/
+
+//Ejercicio 3
+/*for(i=1;i<=12;i++){
+    document.write(i+"x"+"7"+"="+i*7+"<br>");
+}*/
+
+//Ejercicio 4
+/*for (i=1; i<=50; i++){
+    if (i % 2 === 0){
+        console.log(i + " Es un número par.");
+    }else{
+        console.log(i + " Es un número impar.");
+    }
+}*/
+
+//Ejercicio 5
+/*for( i=1; i<=100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        document.write("Pin Pon"+"<br>");
+    }else if(i % 3 === 0){
+        document.write("Pin"+"<br>");
+    }else if(i % 5 === 0){
+        document.write("Pon"+"<br>");
+    }else{
+        document.write(i+"<br>");
+    }
+}*/
+
+//Ejercicio 6
+/*var numero = 0;
+while (numero < 17){
+    numero = Number(prompt("Ingrese un número mayor a 17."));
+}*/
+
+//Ejercicio 7
+/*var punto = "";
+var i = 1;
+while(i<=7){
+    punto += "*";
+    console.log(punto);
+    i++;
+}*/
